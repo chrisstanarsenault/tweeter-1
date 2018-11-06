f2da678fed70ef87bbd2b129ab22524c1d944d3f
Chris <chrisstanarsenault@gmail.com>
2018-11-06 17:17:08 -0500

    added functionality for the textarea counter to count down/up with every character inputted.  Text will turn green and bold if hit 0 limit, and text will turn red if you go over the limit.  Also set code to grab set counter value from the html rather than hardcoding in jquery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  let maxCounter = parseInt($(".counter").text(), 10)
+  $("textarea").keyup(function() {
+    let $textArea = $(this)
+    let myLength = $textArea.val().length;
+    let reduceCounter = maxCounter - myLength;
+    $textArea.siblings(".counter").text(reduceCounter)
+
+    if (reduceCounter < 0) {
+      $textArea.siblings(".counter").css("color", "red")
+      $textArea.siblings(".counter").css("font-size", "1rem")
+      $textArea.siblings(".counter").css("font-weight", "normal")
+    } else if (reduceCounter === 0) {
+      $textArea.siblings(".counter").css("color", "green")
+      $textArea.siblings(".counter").css("font-weight", "bold")
+      $textArea.siblings(".counter").css("font-size", "1.5rem")
+    } else {
+      $textArea.siblings(".counter").css("color", "black")
+      $textArea.siblings(".counter").css("font-weight", "normal")
+      $textArea.siblings(".counter").css("font-size", "1rem")
+    }
+  })
+})
+
