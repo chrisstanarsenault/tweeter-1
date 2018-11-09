6f6aacb7cec95a8c34621976f96cc86e437e494a
Chris <chrisstanarsenault@gmail.com>
2018-11-08 22:29:54 -0500

    connected app to MongoDB and fixed up a couple small CSS issues such as some hovering and padding of the tweet article containers

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,52 +6,52 @@
 
 $(document).ready(function () {
 
-  const data = [{
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
+  // const data = [{
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": {
+  //         "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+  //         "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  //       },
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": {
+  //         "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+  //         "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+  //       },
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Johann von Goethe",
+  //       "avatars": {
+  //         "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+  //         "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+  //       },
+  //       "handle": "@johann49"
+  //     },
+  //     "content": {
+  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+  //     },
+  //     "created_at": 1461113796368
+  //   }
+  // ];
 
   function createTweetElement(tweet) {
     let $tweet = $("<article>").addClass("tweet");
@@ -85,17 +85,20 @@ $(document).ready(function () {
         return "Now"
       }
     }
-    //let tweetContent = $(".tweet").text(tweet.content.text)
-    //let safeHTML = $(".tweet").append(document.createTextNode(tweet.content.text))
+    function escape(str) {
+      var div = document.createElement('div');
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    }
+    const safeHTML = `${escape(tweet.content.text)}`;
 
-    //!!XSS needs fixing still!!
     let html = `
       <header class="tweet-header">
         <img src=${tweet.user.avatars.small} />
         <p class="title-name">${tweet.user.name}</p>
         <p class="title-tweet-handle">${tweet.user.handle}</p>
       </header>
-      <div class = "tweet-body">${tweet.content.text}</div>
+      <div class = "tweet-body">${safeHTML}</div>
       <footer class="tweet-footer">
         <p class="post-time">${timeAgo()}</p>
         <img src="../images/bird.png">
@@ -103,6 +106,7 @@ $(document).ready(function () {
         <img src="../images/bird.png">
       </footer>
       `;
+      $('header').append()
     $tweet = $tweet.append(html);
     return $tweet
   }
@@ -134,10 +138,14 @@ $(document).ready(function () {
       let dataRequest = $tweetSubmit.serialize()
       $.ajax('/tweets', {
         method: 'POST',
-        data: dataRequest
+        data: dataRequest,
+        success: function(data) {
+          renderTweets(data);
+        }
       })
       .then(function () {
         $('textarea').val("") //clear textfield after every submit
+        $('.counter')
         console.log('Done!')
         loadTweets()
         })
